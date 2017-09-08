df445ac072ba98ec59d7cd75b05faf5951d4e404
liuchenxi <lcx010222@163.com>
2017-09-08 10:55:58 +0800

    update

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,6 +1,6 @@
 <template>
 <div id="app">
-    <div class="setting" style="height:24rem;">
+    <div class="setting">
         <div :class="{'menuBox':true,'add':isMenuOpen}">
             <div class="menu">
                 <div class="openMenuBox">
@@ -247,82 +247,83 @@ export default {
                           },
                           success:function(data){
                                   result = data;
+                                  wx.config(result);
+                                  wx.ready(function() {
+
+                                      // 2. 分享接口
+                                      // 2.1 监听“分享给朋友”，按钮点击、自定义分享内容及分享结果接口
+                                      var dataForWeixin = {
+                                          img: "https://image.ppmiao.com/H5picture/common/300.png",
+                                          linkurl: "`,
+                            dataWeixin3: `",title: "`,
+                            dataWeixin4: `",desc: "`,
+                            dataWeixin5: `",};
+                                  wx.onMenuShareAppMessage({
+                                      title: dataForWeixin.title,
+                                      desc: dataForWeixin.desc,
+                                      link: dataForWeixin.linkurl,
+                                      imgUrl: dataForWeixin.img,
+                                      trigger: function(res) {
+
+                                      },
+                                      success: function(res) {
+                                          //alert('已分享');
+                                          //                share();
+                                      },
+                                      cancel: function(res) {
+                                          //alert('已取消');
+                                      },
+                                      fail: function(res) {
+                                          //alert(JSON.stringify(res));
+                                      }
+                                  });
+                                  // 2.2 监听“分享到朋友圈”按钮点击、自定义分享内容及分享结果接口
+                                  wx.onMenuShareTimeline({
+                                      title: dataForWeixin.desc,
+                                      link: dataForWeixin.linkurl,
+                                      imgUrl: dataForWeixin.img,
+                                      trigger: function(res) {
+                                          // alert('用户点击分享到朋友圈');
+                                      },
+                                      success: function(res) {
+                                          //alert('已分享');
+                                          //                share()
+                                      },
+                                      cancel: function(res) {
+                                          //alert('已取消');
+                                      },
+                                      fail: function(res) {
+                                          //alert(JSON.stringify(res));
+                                      }
+                                  });
+
+                                  wx.onMenuShareQQ({
+                                      title: dataForWeixin.title,
+                                      desc: dataForWeixin.desc,
+                                      link: dataForWeixin.linkurl,
+                                      imgUrl: dataForWeixin.img,
+                                      trigger: function(res) {
+                                          //alert('用户点击分享到QQ');
+                                      },
+                                      complete: function(res) {
+                                          //alert(JSON.stringify(res));
+                                      },
+                                      success: function(res) {
+                                          //alert('已分享');
+                                      },
+                                      cancel: function(res) {
+                                          //alert('已取消');
+                                      },
+                                      fail: function(res) {
+                                          //alert(JSON.stringify(res));
+                                      }
+                                  });
+                              });
                                  },
                           dataType: 'json'
                       });
                   });
-                  wx.config(result);
-                  wx.ready(function() {
-
-                      // 2. 分享接口
-                      // 2.1 监听“分享给朋友”，按钮点击、自定义分享内容及分享结果接口
-                      var dataForWeixin = {
-                          img: "https://image.ppmiao.com/H5picture/common/300.png",
-                          linkurl: "`,
-            dataWeixin3: `",title: "`,
-            dataWeixin4: `",desc: "`,
-            dataWeixin5: `",};
-                  wx.onMenuShareAppMessage({
-                      title: dataForWeixin.title,
-                      desc: dataForWeixin.desc,
-                      link: dataForWeixin.linkurl,
-                      imgUrl: dataForWeixin.img,
-                      trigger: function(res) {
-
-                      },
-                      success: function(res) {
-                          //alert('已分享');
-                          //                share();
-                      },
-                      cancel: function(res) {
-                          //alert('已取消');
-                      },
-                      fail: function(res) {
-                          //alert(JSON.stringify(res));
-                      }
-                  });
-                  // 2.2 监听“分享到朋友圈”按钮点击、自定义分享内容及分享结果接口
-                  wx.onMenuShareTimeline({
-                      title: dataForWeixin.desc,
-                      link: dataForWeixin.linkurl,
-                      imgUrl: dataForWeixin.img,
-                      trigger: function(res) {
-                          // alert('用户点击分享到朋友圈');
-                      },
-                      success: function(res) {
-                          //alert('已分享');
-                          //                share()
-                      },
-                      cancel: function(res) {
-                          //alert('已取消');
-                      },
-                      fail: function(res) {
-                          //alert(JSON.stringify(res));
-                      }
-                  });
-
-                  wx.onMenuShareQQ({
-                      title: dataForWeixin.title,
-                      desc: dataForWeixin.desc,
-                      link: dataForWeixin.linkurl,
-                      imgUrl: dataForWeixin.img,
-                      trigger: function(res) {
-                          //alert('用户点击分享到QQ');
-                      },
-                      complete: function(res) {
-                          //alert(JSON.stringify(res));
-                      },
-                      success: function(res) {
-                          //alert('已分享');
-                      },
-                      cancel: function(res) {
-                          //alert('已取消');
-                      },
-                      fail: function(res) {
-                          //alert(JSON.stringify(res));
-                      }
-                  });
-              }); </scr` + `ipt>`
+                   </scr` + `ipt>`
         };
     },
     components: {},
